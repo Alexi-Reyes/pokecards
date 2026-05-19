b000db75e214abad2e962891a541d328285ff2a9
blaise duplantier <blaise.duplantier@gmail.com>
2026-05-19 11:17:52 +0200

    feat: add proxy pattern

--- src/app/api/pokemon/[id]/route.ts
+++ src/app/api/pokemon/[id]/route.ts
@@ -1,5 +1,6 @@
 import { AppConfig } from '@/config';
 import { NextResponse } from 'next/server';
+import { cachedFetch } from '@/proxy/apiCache';
 
 export async function GET(_request: Request, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
@@ -8,13 +9,17 @@ export async function GET(_request: Request, { params }: { params: Promise<{ id:
   }
 
   try {
-    const res = await fetch(`${AppConfig.apiUrl}/pokemon/${id}`);
-    if (res.ok) {
-      const data = await res.json();
-      return NextResponse.json({ data });
-    } else {
+    const data = await cachedFetch(`pokemon:${id}`, async () => {
+      const res = await fetch(`${AppConfig.apiUrl}/pokemon/${id}`);
+      if (!res.ok) return null;
+      return await res.json();
+    });
+
+    if (!data) {
       return NextResponse.json({ error: 'Pokemon not found' }, { status: 404 });
     }
+
+    return NextResponse.json({ data });
   } catch (error) {
     console.error('Error fetching Pokemon:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
